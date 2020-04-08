a3d00ff9a28ecc58ff6a33809441afc1b8f7e6a9
Luke Lonergan <luke@alpaca.markets>
2020-04-08 08:59:07 -0700

    unit tests

---
+++ utils/vector_test.go
@@ -0,0 +1,35 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+
+	"gonum.org/v1/gonum/mat"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVector(t *testing.T) {
+	assert.Equal(t, 123, 123, "should be equal")
+	/*
+		// x = ones(Np)*VX(va) + 0.5*(r+1.)*sT(vc);
+		mm := utils.NewVector(Np).Set(1).ToMatrix().Mul(VX.Subset(va).Transpose())
+		r := utils.Vector{mat.VecDenseCopyOf(R)}
+		X = r.AddScalar(1).Scale(0.5).ToMatrix().Mul(sT.Transpose()).Add(mm).M
+	*/
+	N := 3
+	v1 := NewVector(N).Set(1)
+	require.Equal(t, 1., v1.V.RawVector().Data[N-1])
+	v1.Set(2)
+	require.Equal(t, 2., v1.V.RawVector().Data[N-1])
+
+	M := 2
+	v2 := NewVector(M).Set(3)
+	A := v1.ToMatrix().Mul(v2.Transpose())
+	fmt.Printf("A = \n%v\n", mat.Formatted(A, mat.Squeeze()))
+	nr, nc := A.Dims()
+	require.Equal(t, N, nr)
+	require.Equal(t, M, nc)
+}
