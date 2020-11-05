de5b0e7d0e66876cbf50050b34253136aa8a22c6
Luke Lonergan <luke@alpaca.markets>
2020-11-05 13:04:38 -0800

    Refactor solver divergence test, implement for all supported orders

--- model_problems/Euler2D/euler_test.go
+++ model_problems/Euler2D/euler_test.go
@@ -13,13 +13,6 @@ import (
 
 func TestEuler(t *testing.T) {
 	{ // Test interpolation of solution to edges for all supported orders
-		/*
-			Solver approach:
-			0) Solution is stored on sol points as Q
-			0a) Flux is computed and stored in X, Y component projections in the 2*Nint front of F_RT_DOF
-			1) Solution is extrapolated to edge points in Q_Face from Q
-			2) Edges are traversed, flux is calculated and projected onto edge face normals, scaled and placed into F_RT_DOF
-		*/
 		Nmax := 7
 		for N := 1; N <= Nmax; N++ {
 			c := NewEuler(1, 1, N, "../../DG2D/test_tris_5.neu", FLUX_Average, FREESTREAM, false)
@@ -76,20 +69,22 @@ func TestEuler(t *testing.T) {
 			for k := 0; k < Kmax; k++ {
 				Fr_check[k], Fs_check[k] = c.CalculateFluxTransformed(k, 0, c.Q)
 			}
+			// Interpolate from solution points to edges using precomputed interpolation matrix
+			for n := 0; n < 4; n++ {
+				c.Q_Face[n] = c.dfr.FluxInterpMatrix.Mul(c.Q[n])
+			}
 			// Calculate flux and project into R and S (transformed) directions
-			for i := 0; i < Nint; i++ {
-				for k := 0; k < c.dfr.K; k++ {
-					ind := k + i*Kmax
-					Fr, Fs := c.CalculateFluxTransformed(k, i, c.Q)
-					for n := 0; n < 4; n++ {
+			for n := 0; n < 4; n++ {
+				for i := 0; i < Nint; i++ {
+					for k := 0; k < c.dfr.K; k++ {
+						ind := k + i*Kmax
+						Fr, Fs := c.CalculateFluxTransformed(k, i, c.Q)
 						rtD := c.F_RT_DOF[n].Data()
 						rtD[ind], rtD[ind+Nint*Kmax] = Fr[n], Fs[n]
 					}
 				}
-			}
-			// Check to see that the expected values are in the right place (the internal locations)
-			for k := 0; k < Kmax; k++ {
-				for n := 0; n < 4; n++ {
+				// Check to see that the expected values are in the right place (the internal locations)
+				for k := 0; k < Kmax; k++ {
 					val0, val1 := Fr_check[k][n], Fs_check[k][n]
 					is := k * NpFlux
 					assert.True(t, nearVecScalar(c.F_RT_DOF[n].Transpose().Data()[is:is+Nint],
@@ -98,25 +93,17 @@ func TestEuler(t *testing.T) {
 					assert.True(t, nearVecScalar(c.F_RT_DOF[n].Transpose().Data()[is:is+Nint],
 						val1, 0.000001))
 				}
-			}
-			// Interpolate from solution points to edges using precomputed interpolation matrix
-			for n := 0; n < 4; n++ {
-				c.Q_Face[n] = c.dfr.FluxInterpMatrix.Mul(c.Q[n])
-			}
-			// Set normal flux to a simple addition of the two sides to use as a check in assert()
-			for k := 0; k < Kmax; k++ {
-				for i := 0; i < 3*Nedge; i++ {
-					ind := k + (i+2*Nint)*Kmax
-					Fr, Fs := c.CalculateFluxTransformed(k, i, c.Q_Face)
-					for n := 0; n < 4; n++ {
+				// Set normal flux to a simple addition of the two sides to use as a check in assert()
+				for k := 0; k < Kmax; k++ {
+					for i := 0; i < 3*Nedge; i++ {
+						ind := k + (2*Nint+i)*Kmax
+						Fr, Fs := c.CalculateFluxTransformed(k, i, c.Q_Face)
 						rtD := c.F_RT_DOF[n].Data()
 						rtD[ind] = Fr[n] + Fs[n]
 					}
 				}
-			}
-			// Check to see that the expected values are in the right place (the edge locations)
-			for k := 0; k < Kmax; k++ {
-				for n := 0; n < 4; n++ {
+				// Check to see that the expected values are in the right place (the edge locations)
+				for k := 0; k < Kmax; k++ {
 					val := Fr_check[k][n] + Fs_check[k][n]
 					is := k * NpFlux
 					ie := (k + 1) * NpFlux
@@ -126,7 +113,7 @@ func TestEuler(t *testing.T) {
 			}
 		}
 	}
-	{ // Test solution process part 2
+	{ // Test solution process part 2 - Freestream divergence should be zero
 		Nmax := 7
 		for N := 1; N <= Nmax; N++ {
 			c := NewEuler(1, 1, N, "../../DG2D/test_tris_5.neu", FLUX_Average, FREESTREAM, false)
@@ -135,10 +122,10 @@ func TestEuler(t *testing.T) {
 			c.SetNormalFluxInternal()
 			c.SetNormalFluxOnEdges()
 			// Check that freestream divergence on this mesh is zero
-			for k := 0; k < Kmax; k++ {
-				_, _, Jdet := c.dfr.GetJacobian(k)
-				var div utils.Matrix
-				for n := 0; n < 4; n++ {
+			for n := 0; n < 4; n++ {
+				for k := 0; k < Kmax; k++ {
+					_, _, Jdet := c.dfr.GetJacobian(k)
+					var div utils.Matrix
 					div = c.dfr.FluxElement.DivInt.Mul(c.F_RT_DOF[n])
 					for i := 0; i < Nint; i++ {
 						ind := k + i*Kmax
