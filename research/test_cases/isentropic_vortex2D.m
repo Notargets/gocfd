06ef65356c8450d598ddb27c8f97254e0cacf507
Luke Lonergan <luke@alpaca.markets>
2020-11-06 20:44:37 -0800

    WIP: analytic divergence calculation for isentropic vortex

--- research/test_cases/isentropic_vortex2D.m
+++ research/test_cases/isentropic_vortex2D.m
@@ -1,17 +1,17 @@
 syms x y beta gamma t x0 y0
-r2=(x-t-x0)^2+(y-y0)^2
-bt=beta*exp(1-r2)/(2*pi)
-bt2=bt^2
+r2=(x-t-x0)^2+(y-y0)^2;
+bt=beta*exp(1-r2)/(2*pi);
+bt2=bt^2;
 %isentropic vortex solution for Euler equations in 2D
-u=1-bt*(y-y0)
-v=bt*(x-x0)
-rho=(1-bt2*(gamma-1)/(4*gamma))^(1/(gamma-1))
-q=0.5*rho*(u^2+v^2)
-p=rho^gamma
-E=(p/(gamma-1)+q)/rho % generic calculation of E, not specific to this case, must happen after rho is defined
-F = [ rho*u, rho*u^2+p, rho*u*v, u*(E+p) ]
-G = [ rho*v, rho*u*v, rho*v^2+p, v*(E+p) ]
-div = diff(F,x)+diff(G,y)
+u=1-bt*(y-y0);
+v=bt*(x-x0);
+rho=(1-bt2*(gamma-1)/(4*gamma))^(1/(gamma-1));
+q=0.5*rho*(u^2+v^2);
+p=rho^gamma;
+E=(p/(gamma-1)+q)/rho; % generic calculation of E, not specific to this case, must happen after rho is defined
+F = [ rho*u, rho*u^2+p, rho*u*v, u*(E+p) ];
+G = [ rho*v, rho*u*v, rho*v^2+p, v*(E+p) ];
+div = diff(F,x)+diff(G,y);
 disp(ccode(div(1)));
 disp(ccode(div(2)));
 disp(ccode(div(3)));
