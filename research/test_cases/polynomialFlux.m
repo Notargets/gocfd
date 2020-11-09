5c020b87102a6633ca68736b82914dedf20e1b65
Luke Lonergan <luke@alpaca.markets>
2020-11-09 09:57:04 -0800

    re-work of the polynomial test flux - only real components

--- research/test_cases/polynomialFlux.m
+++ research/test_cases/polynomialFlux.m
@@ -1,7 +1,7 @@
-syms a b c d plotRho(x,y) plotDiv(x,y) gamma
+syms a b c d x y gamma
 %2D Polynomial field
-rho(x,y)=a*x/2*pi+b*y/2*pi;
-u = c*sin(x/2*pi); v = d*cos(y/2*pi);
+rho=a*abs(x)+b*abs(y);
+u = c*x; v = d*y;
 rhou=rho*u; rhov=rho*v;
 p=rho^gamma;
 q=0.5*rho*(u^2+v^2);
@@ -14,15 +14,26 @@ fprintf('Code for Divergence of F and G Fluxes\n%s\n',ccode(div));
 fprintf('Code for U \n%s\n%s\n%s\n%s\n',ccode(U));
 plotRho(x,y) = subs(rho,[a,b],[1.,1.]);
 plotDiv(x,y) = subs(div,[a,b,c,d,gamma],[1.,1.,1.,1.,1.4]);
-x = linspace(-2*pi,2*pi);
-y = linspace(0,4*pi);
+plotU(x,y) = subs(U,[a,b,c,d,gamma],[1.,1.,1.,1.,1.4]);
+subs(U(4),[a,b,c,d,gamma],[1.,1.,1.,1.,1.4])
+x = linspace(-10,10,20);
+y = linspace(-10,10,20);
 [X,Y] = meshgrid(x,y);
-Z = plotDiv(X,Y);
-figure(1)
-contour(X,Y,real(Z{1}),10, 'ShowText','on')
-% figure(2)
-% contour(X,Y,real(Z{2}),10, 'ShowText','on')
-% figure(3)
-% contour(X,Y,real(Z{3}),10, 'ShowText','on')
-% figure(4)
-% contour(X,Y,real(Z{4}),10, 'ShowText','on')
+Z = plotU(X,Y);
+figure('Name','rho','NumberTitle','off');
+contour(X,Y,double(Z{1}),10, 'ShowText','on');
+figure('Name','rhoU','NumberTitle','off');
+contour(X,Y,double(Z{2}),10, 'ShowText','on');
+figure('Name','rhoV','NumberTitle','off');
+contour(X,Y,double(Z{3}),10, 'ShowText','on');
+figure('Name','E','NumberTitle','off');
+contour(X,Y,double(Z{4}),10, 'ShowText','on');
+divZ = plotDiv(X,Y);
+figure('Name','Divergence of Rho','NumberTitle','off');
+contour(X,Y,double(divZ{1}),10, 'ShowText','on');
+figure('Name','Divergence of RhoU','NumberTitle','off');
+contour(X,Y,double(divZ{2}),10, 'ShowText','on');
+figure('Name','Divergence of RhoV','NumberTitle','off');
+contour(X,Y,double(divZ{3}),10, 'ShowText','on');
+figure('Name','Divergence of E','NumberTitle','off');
+contour(X,Y,double(divZ{4}),10, 'ShowText','on');
