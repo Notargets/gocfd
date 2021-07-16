1c431dca554fd9dae17d2ae58b7337c9246a7e1c
Luke Lonergan <mynameis2056@yahoo.com>
2021-07-16 09:56:59 -0700

    fixed change of variables

--- MATLAB/preconditioner2.m
+++ MATLAB/preconditioner2.m
@@ -1,6 +1,6 @@
 syms gamma rho rhoU rhoV E a;
 U = [ rho, rhoU, rhoV, E]; 
-W = [ (gamma-1)*(E- (1/(2*rho))*(rhoU^2+rhoV^2)), rhoU/rho, rhoV/rho, (gamma-1)*(E- (1/(2*rho))*(rhoU^2+rhoV^2))-rho*a^2];
+W = [ (gamma-1)*(E- (1/(2*rho))*(rhoU^2+rhoV^2)), rhoU/rho, rhoV/rho, ((gamma-1)/(rho^gamma))*(E- (1/(2*rho))*(rhoU^2+rhoV^2))];
 dWdU = jacobian(W,U);
 dUdW = inv(dWdU);
 disp("dUdW Jacobian");
@@ -15,7 +15,7 @@ P0 = [
 disp("pressure coordinates preconditioner");
 disp(P0);
 
-P0t = simplify(dUdW*P0);
+P0t = simplify(dUdW*P0*dWdU);
 
 disp("transformed preconditioner in conservative variables");
 disp(P0t);
