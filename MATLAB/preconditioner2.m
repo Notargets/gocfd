1c16439ee04120138b0c8a5f72cd06dbe68e3aee
Luke Lonergan <mynameis2056@yahoo.com>
2021-07-16 17:02:38 -0700

    WIP: Comparison to Turkel paper's jacobian of dWdU shows last line is off by (1/p), eg: instead of (gamma-1) we have (gamma-1)/p

--- MATLAB/preconditioner2.m
+++ MATLAB/preconditioner2.m
@@ -1,18 +1,31 @@
-syms gamma rho rhoU rhoV E a;
-U = [ rho, rhoU, rhoV, E]; 
-W = [ (gamma-1)*(E- (1/(2*rho))*(rhoU^2+rhoV^2)), rhoU/rho, rhoV/rho, ((gamma-1)/(rho^gamma))*(E- (1/(2*rho))*(rhoU^2+rhoV^2))];
+syms gamma rho rhoU rhoV E;
+U = [ rho, rhoU, rhoV, E];
+q = (1/(2*rho))*(rhoU^2+rhoV^2);
+p = (gamma-1)*(E-q);
+%S = log(p / (rho^gamma));
+S = log(p/(rho^gamma));
+%S = log(p) - gamma*log(rho);
+W = [p,rhoU/rho,rhoV/rho,S];
 dWdU = jacobian(W,U);
+disp("dWdU Jacobian");
+disp(dWdU);
+
+error("planned");
+
 dUdW = inv(dWdU);
 disp("dUdW Jacobian");
 disp(dUdW);
-syms alpha u v BMr2Oa2 delta;
+
+syms alpha a BMr2Oa2 delta;
+a = sqrt(gamma*p/rho);
 P0 = [
     BMr2Oa2, 0, 0, -delta*BMr2Oa2;
-    -alpha*u/(rho*a^2), 1, 0, delta*alpha*u/(rho*a^2);
-    -alpha*v/(rho*a^2), 0, 1, delta*alpha*v/(rho*a^2);
+    -alpha*rhoU/(rho^2*a^2), 1, 0, delta*alpha*rhoU/(rho^2*a^2);
+    -alpha*rhoV/(rho^2*a^2), 0, 1, delta*alpha*rhoV/(rho^2*a^2);
     0, 0, 0, 1;
     ];
 disp("pressure coordinates preconditioner");
+P0 = simplify(P0);
 disp(P0);
 
 P0t = simplify(dUdW*P0*dWdU);
@@ -21,4 +34,5 @@ disp("transformed preconditioner in conservative variables");
 disp(P0t);
 
 disp("source code for transformed preconditioner");
+
 ccode(P0t)
