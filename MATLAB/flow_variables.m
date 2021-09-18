680f7478cd9f939d74daeba021d87daf61ef8bc9
Luke Lonergan <mynameis2056@yahoo.com>
2021-09-17 20:18:06 -0700

    cleanup

--- MATLAB/flow_variables.m
+++ MATLAB/flow_variables.m
@@ -2,15 +2,16 @@ syms rho u e0 E0 T Cp Cv Gamma M;
 U = [rho rho*u rho*e0];
 U = [rho rho*u E0];
 % Perfect Gas
-R = Cp - Cv;
-Gamma = Cp/Cv;
+% R = Cp - Cv;
+R = 1;
+%Gamma = Cp/Cv;
 P = rho*R*T;
 % Specific Energy
-e = e0 - u^2/2;
 e = Cv*T;
+e = str2sym('e0 - u^2/2');
 % Pressure
-q = (1/2)*rho*u^2;
-P = (Gamma-1)*(rho*e0-q);
+P = str2sym('(E0 - q)*(Gamma - 1)');
+q = (rho*u^2)/2;
 % Stagnation Enthalpy
 h0 = e0 + P/rho;
 % Specific Enthalpy
@@ -21,6 +22,7 @@ P0 = P*((1+(Gamma-1)/2)*M^2)^(Gamma/(Gamma-1));
 T0 = T + (q/(rho*Cp));
 % Speed of Sound
 c = sqrt(Gamma*R*T);
+c = str2sym('sqrt(Gamma*P/rho)');
 % Mach Number
 M = u/c;
 % Enthalpy and Total Enthalpy
