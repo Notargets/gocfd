e4a4642b12c94b33ffc70dc0984138aa4c53a152
Luke Lonergan <mynameis2056@yahoo.com>
2021-10-18 10:37:20 -0700

    refactor edge calculations

--- model_problems/Euler2D/edges.go
+++ model_problems/Euler2D/edges.go
@@ -109,6 +109,15 @@ func (nf *EdgeValueStorage) GetEdgeValues(valType ValueType, myThread, kLocal, v
 	return
 }
 
+func (c *Euler) GetFaceNormal(kGlobal, edgeNumber int) (normal [2]float64) {
+	var (
+		KmaxGlobal = c.dfr.K
+		faceInd    = kGlobal + KmaxGlobal*edgeNumber
+	)
+	normal = [2]float64{c.dfr.FaceNorm[0].DataP[faceInd], c.dfr.FaceNorm[1].DataP[faceInd]}
+	return
+}
+
 func (c *Euler) CalculateNormalFlux(Time float64, CalculateDT bool, Jdet, DT []utils.Matrix, Q_Face [][4]utils.Matrix,
 	edgeKeys EdgeKeySlice, EdgeQ1, EdgeQ2 [][4]float64) (waveSpeedMax float64) {
 	var (
@@ -116,50 +125,34 @@ func (c *Euler) CalculateNormalFlux(Time float64, CalculateDT bool, Jdet, DT []u
 		normalFlux  = EdgeQ1
 		avgSolution = EdgeQ2
 		pm          = c.Partitions
-		KmaxGlobal  = c.dfr.K
 	)
 	for _, en := range edgeKeys {
 		e := c.dfr.Tris.Edges[en]
 		var (
-			k0Global    = int(e.ConnectedTris[0])
-			edgeNumber0 = int(e.ConnectedTriEdgeNumber[0])
-			faceInd     = k0Global + KmaxGlobal*edgeNumber0
-			normal0     = [2]float64{c.dfr.FaceNorm[0].DataP[faceInd], c.dfr.FaceNorm[1].DataP[faceInd]}
+			k0Global             = int(e.ConnectedTris[0])
+			k0, Kmax0, myThread0 = pm.GetLocalK(int(e.ConnectedTris[0]))
+			edgeNumber0          = int(e.ConnectedTriEdgeNumber[0])
+			normal0              = c.GetFaceNormal(k0Global, edgeNumber0)
 		)
 		switch e.NumConnectedTris {
 		case 0:
 			panic("unable to handle unconnected edges")
 		case 1: // Handle edges with only one triangle - default is edge flux, which will be replaced by a BC flux
-			var (
-				k, Kmax, bn = pm.GetLocalK(int(e.ConnectedTris[0]))
-				edgeNumber  = int(e.ConnectedTriEdgeNumber[0])
-			)
-			c.calculateBCs(e, k, Kmax, bn, edgeNumber, Nedge, normal0, Q_Face, Time, normalFlux, avgSolution)
+			c.calculateBCs(e, Nedge, Time,
+				k0, Kmax0, edgeNumber0, myThread0,
+				normal0, normalFlux, avgSolution, Q_Face)
 		case 2: // Handle edges with two connected tris - shared faces
 			var (
-				kL, KmaxL, bnL           = pm.GetLocalK(int(e.ConnectedTris[0]))
-				kR, KmaxR, bnR           = pm.GetLocalK(int(e.ConnectedTris[1]))
-				edgeNumberL, edgeNumberR = int(e.ConnectedTriEdgeNumber[0]), int(e.ConnectedTriEdgeNumber[1])
-				shiftL, shiftR           = edgeNumberL * Nedge, edgeNumberR * Nedge
+				kR, KmaxR, myThreadR = pm.GetLocalK(int(e.ConnectedTris[1]))
+				edgeNumberR          = int(e.ConnectedTriEdgeNumber[1])
 			)
-			switch c.FluxCalcAlgo {
-			case FLUX_Average:
-				c.AvgFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[bnL], Q_Face[bnR], normal0, normalFlux)
-			case FLUX_LaxFriedrichs:
-				c.LaxFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[bnL], Q_Face[bnR], normal0, normalFlux)
-			case FLUX_Roe:
-				c.RoeFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[bnL], Q_Face[bnR], normal0, normalFlux)
-			case FLUX_RoeER:
-				c.RoeERFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[bnL], Q_Face[bnR], normal0, normalFlux)
-			}
-			// Store the average solution for this edge - average of the two connected tri edges
-			for i := 0; i < Nedge; i++ {
-				indL := kL + (i+shiftL)*KmaxL
-				indR := kR + (Nedge-1-i+shiftR)*KmaxR
-				for n := 0; n < 4; n++ {
-					avgSolution[i][n] = 0.5 * (Q_Face[bnL][n].DataP[indL] + Q_Face[bnR][n].DataP[indR])
-				}
-			}
+			c.calculateNumericalFlux(Nedge,
+				k0, Kmax0, edgeNumber0, myThread0,
+				kR, KmaxR, edgeNumberR, myThreadR,
+				normal0, normalFlux, avgSolution, Q_Face)
+		}
+		if CalculateDT {
+			waveSpeedMax = c.calculateLocalDT(e, Nedge, Q_Face, Jdet, DT)
 		}
 		// Load the normal flux into the global normal flux storage
 		edgeIndex := c.EdgeStore.StorageIndex[en]
@@ -170,47 +163,77 @@ func (c *Euler) CalculateNormalFlux(Time float64, CalculateDT bool, Jdet, DT []u
 				c.EdgeStore.EdgeSolutionStorage[n].DataP[ind] = avgSolution[i][n]
 			}
 		}
-		if CalculateDT {
-			var (
-				Np1  = c.dfr.N + 1
-				Np12 = float64(Np1 * Np1)
-			)
-			var (
-				conn        = 0
-				edgeNum     = int(e.ConnectedTriEdgeNumber[conn])
-				shift       = edgeNum * Nedge
-				edgeLen     = e.GetEdgeLength()
-				k, Kmax, bn = pm.GetLocalK(int(e.ConnectedTris[conn]))
-			)
-			// fmt.Printf("N, Np12, edgelen, Jdet = %d,%8.5f,%8.5f,%8.5f\n", c.dfr.N, Np12, edgeLen, Jdet)
-			fs := 0.5 * Np12 * edgeLen / Jdet[bn].DataP[k]
-			edgeMax := -100.
-			for i := shift; i < shift+Nedge; i++ {
-				ind := k + i*Kmax
-				C := c.FS.GetFlowFunction(Q_Face[bn], ind, SoundSpeed)
-				U := c.FS.GetFlowFunction(Q_Face[bn], ind, Velocity)
-				waveSpeed := fs * (U + C)
-				waveSpeedMax = math.Max(waveSpeed, waveSpeedMax)
-				if waveSpeed > edgeMax {
-					edgeMax = waveSpeed
-				}
-			}
-			if edgeMax > DT[bn].DataP[k] {
-				DT[bn].DataP[k] = edgeMax
-			}
-			if e.NumConnectedTris == 2 { // Add the wavespeed to the other tri connected to this edge if needed
-				k, Kmax, bn = pm.GetLocalK(int(e.ConnectedTris[1]))
-				if edgeMax > DT[bn].DataP[k] {
-					DT[bn].DataP[k] = edgeMax
-				}
-			}
+	}
+	return
+}
+
+func (c *Euler) calculateLocalDT(e *DG2D.Edge, Nedge int,
+	Q_Face [][4]utils.Matrix, Jdet, DT []utils.Matrix) (waveSpeedMax float64) {
+	var (
+		pm                = c.Partitions
+		edgeNum           = int(e.ConnectedTriEdgeNumber[0])
+		k, Kmax, myThread = pm.GetLocalK(int(e.ConnectedTris[0]))
+		Np1               = c.dfr.N + 1
+		Np12              = float64(Np1 * Np1)
+		shift             = edgeNum * Nedge
+		edgeLen           = e.GetEdgeLength()
+	)
+	fs := 0.5 * Np12 * edgeLen / Jdet[myThread].DataP[k]
+	edgeMax := -100.
+	for i := shift; i < shift+Nedge; i++ {
+		ind := k + i*Kmax
+		C := c.FS.GetFlowFunction(Q_Face[myThread], ind, SoundSpeed)
+		U := c.FS.GetFlowFunction(Q_Face[myThread], ind, Velocity)
+		waveSpeed := fs * (U + C)
+		waveSpeedMax = math.Max(waveSpeed, waveSpeedMax)
+		if waveSpeed > edgeMax {
+			edgeMax = waveSpeed
+		}
+	}
+	if edgeMax > DT[myThread].DataP[k] {
+		DT[myThread].DataP[k] = edgeMax
+	}
+	if e.NumConnectedTris == 2 { // Add the wavespeed to the other tri connected to this edge if needed
+		k, Kmax, myThread = pm.GetLocalK(int(e.ConnectedTris[1]))
+		if edgeMax > DT[myThread].DataP[k] {
+			DT[myThread].DataP[k] = edgeMax
 		}
 	}
 	return
 }
 
-func (c *Euler) calculateBCs(e *DG2D.Edge, k, Kmax, myThread, edgeNumber, Nedge int,
-	normal0 [2]float64, Q_Face [][4]utils.Matrix, Time float64, normalFlux, avgSolution [][4]float64) {
+func (c *Euler) calculateNumericalFlux(Nedge, kL, KmaxL, edgeNumberL, myThreadL, kR, KmaxR, edgeNumberR, myThreadR int,
+	normal0 [2]float64, normalFlux, avgSolution [][4]float64,
+	Q_Face [][4]utils.Matrix) {
+	var (
+		shiftL, shiftR = edgeNumberL * Nedge, edgeNumberR * Nedge
+	)
+	switch c.FluxCalcAlgo {
+	case FLUX_Average:
+		c.AvgFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[myThreadL], Q_Face[myThreadR], normal0, normalFlux)
+	case FLUX_LaxFriedrichs:
+		c.LaxFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[myThreadL], Q_Face[myThreadR], normal0, normalFlux)
+	case FLUX_Roe:
+		c.RoeFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[myThreadL], Q_Face[myThreadR], normal0, normalFlux)
+	case FLUX_RoeER:
+		c.RoeERFlux(kL, kR, KmaxL, KmaxR, shiftL, shiftR, Q_Face[myThreadL], Q_Face[myThreadR], normal0, normalFlux)
+	}
+	// Store the average solution for this edge - average of the two connected tri edges
+	for i := 0; i < Nedge; i++ {
+		indL := kL + (i+shiftL)*KmaxL
+		indR := kR + (Nedge-1-i+shiftR)*KmaxR
+		for n := 0; n < 4; n++ {
+			avgSolution[i][n] = 0.5 * (Q_Face[myThreadL][n].DataP[indL] + Q_Face[myThreadR][n].DataP[indR])
+		}
+	}
+}
+
+//func (c *Euler) calculateBCs(e *DG2D.Edge, k, Kmax, myThread, edgeNumber, Nedge int,
+//normal0 [2]float64, Q_Face [][4]utils.Matrix, Time float64, normalFlux, avgSolution [][4]float64) {
+func (c *Euler) calculateBCs(e *DG2D.Edge, Nedge int, Time float64,
+	k, Kmax, edgeNumber, myThread int,
+	normal0 [2]float64, normalFlux, avgSolution [][4]float64,
+	Q_Face [][4]utils.Matrix) {
 	var (
 		calculateNormalFlux bool
 		shift               = edgeNumber * Nedge
