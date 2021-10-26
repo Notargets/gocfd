9e757a14bccc72bf063a207db95fbba415888665
Luke Lonergan <mynameis2056@yahoo.com>
2021-10-26 10:31:38 -0700

    update

--- test_cases/Euler2D/naca_12/runme.sh
+++ test_cases/Euler2D/naca_12/runme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gocfd 2D -I input-wall.yaml -F mesh/nacaAirfoil-base.su2 -g -s20 -q 4 -z 0.06 -x .25 -y .25 -k 0.2 -l 1.6
+gocfd 2D -I input-wall.yaml -F mesh/nacaAirfoil-base.su2 -g -s20 -q 4 -z 0.06 -x .25 -y .25 -k 0.3 -l 1.5
