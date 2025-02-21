60d18f798cd67d83499ac7e31b96251337c033e3
Luke Lonergan <mynameis2056@yahoo.com>
2025-02-21 13:08:32 -0800

    Add mesh plotting in test

--- go.mod
+++ go.mod
@@ -18,12 +18,17 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20240506104042-037f3cc74f2a // indirect
+	github.com/go-gl/mathgl v1.2.0 // indirect
 	github.com/gonum/floats v0.0.0-20181209220543-c233463c7e82 // indirect
 	github.com/gonum/internal v0.0.0-20181124074243-f884aa714029 // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.1 // indirect
 	github.com/mitchellh/mapstructure v1.3.2 // indirect
+	github.com/notargets/avs v0.0.8-0.20250116180827-ede1da557836 // indirect
 	github.com/pelletier/go-toml v1.8.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.2.2 // indirect
@@ -31,6 +36,7 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/image v0.23.0 // indirect
 	golang.org/x/sys v0.14.0 // indirect
 	golang.org/x/text v0.21.0 // indirect
 	gopkg.in/ini.v1 v1.57.0 // indirect
--- model_problems/Burgers2D/burgers_test.go
+++ model_problems/Burgers2D/burgers_test.go
@@ -1,7 +1,20 @@
 package Burgers2D
 
 import (
+	"encoding/binary"
+	"fmt"
+	"math"
+	"os"
+	"strings"
 	"testing"
+	"time"
+
+	"github.com/notargets/avs/chart2d"
+	avsUtils "github.com/notargets/avs/utils"
+
+	"github.com/notargets/gocfd/utils"
+
+	"github.com/notargets/avs/geometry"
 
 	"github.com/notargets/gocfd/DG2D"
 )
@@ -11,9 +24,146 @@ func TestNewBurgers2D(t *testing.T) {
 	b2d := &Burgers2D{}
 	mesh := "../../DG2D/test_data/test_tris_9.neu"
 	b2d.DFR = DG2D.NewDFR2D(P, true, mesh)
-	b2d.DFR.VX.Transpose().Print("VX")
-	b2d.DFR.VY.Transpose().Print("VY")
-	// cc := chart2d.NewChart2D(1920, 1080, 0, 10, 0, 10)
-	// cc.AddTriMesh()
+	if testing.Verbose() {
+		plotMesh(b2d.DFR, 30*time.Second)
+	}
+	return
+}
+
+func plotMesh(dfr *DG2D.DFR2D, waitTime time.Duration) {
+	var xmin, ymin, xmax, ymax float64
+	xmin, xmax = math.MaxFloat64, -math.MaxFloat64
+	ymin, ymax = math.MaxFloat64, -math.MaxFloat64
+
+	pMesh := makePlotMesh(dfr.VX, dfr.VY, dfr.Tris)
+
+	for i, x := range dfr.VX.DataP {
+		y := dfr.VY.AtVec(i)
+		if x < xmin {
+			xmin = x
+		}
+		if x > xmax {
+			xmax = x
+		}
+		if y < ymin {
+			ymin = y
+		}
+		if y > ymax {
+			ymax = y
+		}
+	}
+	xMin, xMax, yMin, yMax := getSquareBoundingBox(float32(xmin),
+		float32(xmax), float32(ymin), float32(ymax))
+	cc := chart2d.NewChart2D(xMin, xMax, yMin, yMax, 1920, 1080,
+		avsUtils.WHITE, avsUtils.BLACK, 0.9)
+	// cc.NewWindow("Geom", 0.9, screen.AUTO)
+	cc.AddTriMesh(pMesh)
+	time.Sleep(waitTime)
+}
+
+func makePlotMesh(VX, VY utils.Vector, Tris *DG2D.Triangulation) (tMesh geometry.TriMesh) {
+	var (
+		K = len(Tris.EtoE)
+	)
+	tMesh = geometry.TriMesh{
+		XY:       make([]float32, 2*VX.Len()),
+		TriVerts: make([][3]int64, K),
+	}
+	for i, x := range VX.DataP {
+		y := VY.DataP[i]
+		tMesh.XY[2*i] = float32(x)
+		tMesh.XY[2*i+1] = float32(y)
+	}
+	for k := 0; k < K; k++ {
+		for n := 0; n < 3; n++ {
+			tMesh.TriVerts[k][n] = int64(Tris.EToV.At(k, n))
+		}
+	}
+	return
+}
+
+func getSurfaceLines(XY []float32, edges []*geometry.EdgeGroup) (
+	XYSurf []float32) {
+	var x1, y1, x2, y2 float32
+	for _, edgeGroup := range edges {
+		// fmt.Printf("BC Group Name: [%s]\n", edgeGroup.GroupName)
+		// if edgeGroup.GroupName == "wall" {
+		for _, edgeXY := range edgeGroup.EdgeXYs {
+			x1, y1 = edgeXY[0], edgeXY[1]
+			x2, y2 = edgeXY[2], edgeXY[3]
+			XYSurf = append(XYSurf, x1, y1, x2, y2)
+		}
+		// }
+	}
+	return
+}
+
+func getSquareBoundingBox(xMin, xMax, yMin, yMax float32) (xBMin,
+	xBMax, yBMin, yBMax float32) {
+	xRange := xMax - xMin
+	yRange := yMax - yMin
+	if yRange > xRange {
+		yBMin = yMin
+		yBMax = yMax
+		xCent := xRange/2. + xMin
+		xBMin = xCent - yRange/2.
+		xBMax = xCent + yRange/2.
+	} else {
+		xBMin = xMin
+		xBMax = xMax
+		yCent := yRange/2. + yMin
+		yBMin = yCent - xRange/2.
+		yBMax = yCent + xRange/2.
+	}
+	return
+}
+
+func ReadGoCFDMesh(path string, verbose bool) (tMesh geometry.TriMesh,
+	BCEdges []*geometry.EdgeGroup) {
+	var (
+		file        *os.File
+		lenTriVerts int64
+		lenXYCoords int64
+	)
+
+	// Read triVerts array
+	triVerts := make([]int64, lenTriVerts)
+	binary.Read(file, binary.LittleEndian, &triVerts)
+
+	// Read XY coordinates
+	binary.Read(file, binary.LittleEndian, &lenXYCoords)
+	fmt.Printf("Number of Coordinates: %d\n", lenXYCoords)
+	xy := make([]float64, lenXYCoords*2)
+	binary.Read(file, binary.LittleEndian, &xy)
+
+	xy32 := make([]float32, lenXYCoords*2)
+	for i := range xy {
+		xy32[i] = float32(xy[i])
+	}
+
+	verts := make([][3]int64, lenTriVerts/3)
+	for i := range verts {
+		verts[i][0] = triVerts[3*i]
+		verts[i][1] = triVerts[3*i+1]
+		verts[i][2] = triVerts[3*i+2]
+	}
+	tMesh = *geometry.NewTriMesh(xy32, verts)
+
+	var nBCs int64
+	binary.Read(file, binary.LittleEndian, &nBCs)
+	BCEdges = make([]*geometry.EdgeGroup, nBCs)
+	fmt.Printf("Number of BCs: %d\n", nBCs)
+	// Read boundary conditions
+	for n := 0; n < int(nBCs); n++ {
+		var fString [16]byte
+		binary.Read(file, binary.LittleEndian, &fString)
+		bcName := strings.TrimRight(string(fString[:]), "\x00 ")
+		var bcLen int64
+		binary.Read(file, binary.LittleEndian, &bcLen)
+		BCEdges[n] = geometry.NewEdgeGroup(bcName, int(bcLen))
+		for i, _ := range BCEdges[n].EdgeXYs {
+			binary.Read(file, binary.LittleEndian, &BCEdges[n].EdgeXYs[i])
+		}
+	}
 	return
 }
