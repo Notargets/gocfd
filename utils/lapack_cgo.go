16a740f9dbd680e2609266633dbb96c24079665d
Luke Lonergan <mynameis2056@yahoo.com>
2025-04-28 10:57:24 -0700

    cleanup netlib

--- utils/lapack_cgo.go
+++ utils/lapack_cgo.go
@@ -4,22 +4,21 @@
 package utils
 
 /*
-#cgo CFLAGS: -O3 -march=native -mavx -mavx2 -mfma
+#cgo CFLAGS: -march=native -mavx -mavx2
 #cgo LDFLAGS: -lopenblas -llapacke -lgfortran -lm -lpthread
 #include <cblas.h>
 #include <lapacke.h>
 */
-
 import "C"
 
 import (
+	"fmt"
+
 	"gonum.org/v1/gonum/blas/blas64"
-	"gonum.org/v1/gonum/lapack/lapack64"
 	netblas "gonum.org/v1/netlib/blas/netlib"
-	netlapack "gonum.org/v1/netlib/lapack/netlib"
 )
 
 func init() {
 	blas64.Use(netblas.Implementation{})
-	lapack64.Use(netlapack.Implementation{})
+	fmt.Println("Using netlib to accelerate BLAS")
 }
