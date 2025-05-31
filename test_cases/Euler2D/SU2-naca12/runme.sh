8385a648a6510b8a4a3fe64874e62411d48510a2
Luke Lonergan <mynameis2056@yahoo.com>
2025-05-31 12:54:33 -0700

    Add Stanford SU2 Naca Airfoil Test Case

---
+++ test_cases/Euler2D/SU2-naca12/runme.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#gocfd 2D -I input-wall.yaml -F mesh/nacaAirfoil-base.su2
+for order in 0 1 2 3 4
+#for order in 4
+do
+	cat input-base.yaml > tmp.yaml
+	echo "PolynomialOrder: $order" >> tmp.yaml
+	if [ $order -eq 0 ]; then
+		echo "CFL: 2.0" >> tmp.yaml
+		echo "MaxIterations: 6000" >> tmp.yaml
+	elif [ $order -eq 1 ]; then
+		echo "CFL: 2.0" >> tmp.yaml
+		echo "MaxIterations: 10000" >> tmp.yaml
+	elif [ $order -eq 4 ]; then
+		echo "CFL: 2.5" >> tmp.yaml
+		echo "MaxIterations: 10000" >> tmp.yaml
+	fi
+	gocfd 2D -I tmp.yaml -F mesh_NACA0012_inv.su2 >& sysout-$order
+    if [ -f meshfile.gobcfd ]; then
+    	mv meshfile.gobcfd meshfile-$order.gobcfd
+    	mv solutionfile.gobcfd solutionfile-$order.gobcfd
+	fi
+done
